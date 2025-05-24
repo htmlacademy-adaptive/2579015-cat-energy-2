689e454857c23c56ec9fef3077e512b96272d1bd
arevik-antonyan <arevik.antonyan98@gmail.com>
2025-05-24 11:58:42 +0300

    Add decor elements (#8)
    * Добавляет декоративные элементы
    
    * Добавляет корректировки
    
    * lint check
    
    * Добавляет корректировки №2
    
    * check lint

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,35 @@
-/* в этот файл добавляет скрипты*/
+/* Логика открытия навигации */
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+/* Подстановка кастомного маркера в карту */
+const ymaps = window.ymaps;
+ymaps.ready(() => {
+  const mapElement1 = document.getElementById('map');
+  if(mapElement1) {
+    const myMap = new ymaps.Map(mapElement1, {
+      center: [59.938679, 30.3230044],
+      zoom: 16,
+      controls: []
+    });
+    const myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../images/map/pin.png',
+      iconImageSize: [57, 53],
+      iconImageOffset: [-26, -42]
+    });
+    myMap.geoObjects.add(myPlacemark);
+  }
+});
